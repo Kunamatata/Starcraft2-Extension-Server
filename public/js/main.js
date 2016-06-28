881c3d8e20ed8fc826a196f67fc87c4599736552
Max <max.caly@gmail.com>
2016-06-28 11:58:06 -0600

    Updated styling

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+'use strict'
+
+let sc2ApiStatus = ''
+let apiIconCheck = document.getElementById('icon-api-check')
+let apiIconError = document.getElementById('icon-api-error')
+let body = document.querySelector('body')
+let bgArray = ['bg-1.jpg', 'bg-2.jpg', 'bg-3.jpg']
+let httpRequest = new XMLHttpRequest()
+httpRequest.onreadystatechange = checkStatus
+
+function sendRequest() {
+    httpRequest.open('GET', 'http://kuna-starcraft2.rhcloud.com/api/status', true)
+    httpRequest.send(null);
+}
+
+function checkStatus() {
+    if (httpRequest.status === 200) {
+        apiIconCheck.style.display = 'block'
+        apiIconError.style.display = 'none'
+    } else {
+        apiIconCheck.style.display = 'none'
+        apiIconError.style.display = 'block'
+    }
+}
+setInterval(sendRequest, 600000)
+
+function changeBGImg() {
+    var random = Math.floor(Math.random() * 3)
+    body.style.backgroundImage = `url(../img/${bgArray[random]})`
+}
+
+setInterval(changeBGImg, 10000)
