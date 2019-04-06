00a0545b29d7f3dc8ca5e13758e5031fbaed72d0
Maxime Caly <max.caly@gmail.com>
2019-04-05 18:06:02 -0700

    Add Cache-Control for API resources

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ app.use(favicon(path.join(__dirname, 'public/img', 'bg-1.jpg')));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Cache-Control', 'max-age=20');
   next();
 });
 
